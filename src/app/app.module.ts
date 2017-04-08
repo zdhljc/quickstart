7d54654f5d197dfd01105288b3b1b2e5bf07f16b
Paul <279141102@qq.com>
2017-04-08 15:18:08 +0800

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,31 @@ import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+  path: 'dashboard',
+  component: DashboardComponent
+  },
+{
+  path: '',
+  redirectTo: '/dashboard',
+  pathMatch: 'full'
+},
+])],
+  declarations: [ AppComponent, HeroDetailComponent,HeroesComponent,DashboardComponent ],
+  providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
